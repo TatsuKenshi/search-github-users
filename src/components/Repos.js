7eb02ac63598ae1ad4e225eed6d5305f499ba42b
TatsuKenshi <rockinbg@gmail.com>
2022-10-23 03:50:33 +0200

    adds an example chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./charts";
 
 const Repos = () => {
-  return (
-    <div>Repos</div>
-  )
-}
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
 
-export default Repos
+  // STEP 2 - Chart Data
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "60",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <div className="section-center">
+        <ExampleChart data={chartData} />
+      </div>
+    </section>
+  );
+};
+
+export default Repos;
