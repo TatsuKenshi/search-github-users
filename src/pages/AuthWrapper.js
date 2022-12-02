7b8162bc6f6a0ec43b2f45644d1201dcc589f082
TatsuKenshi <rockinbg@gmail.com>
2022-12-02 14:18:11 +0100

    loading spinners adjusted

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import loadingGif from "../images/preloader.gif";
+import "../styles/pages/AuthWrapper.scss";
 
 const AuthWrapper = ({ children }) => {
   const { isLoading, error } = useAuth0();
 
   if (isLoading) {
     return (
-      <div>
-        <img src={loadingGif} alt="spinner" />
+      <div className="loading-div">
+        <img src={loadingGif} alt="loading spinner" className="loading-img" />
       </div>
     );
   }
